36e950efbb27a6c008c4c31d44d945da7f5a0385
Jiawei <liujiaweidreamer@gmail.com>
2020-09-22 17:55:23 +0800

    CRM - Modify and Run

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 const Koa = require("koa");
 
 const app = new Koa();
+const port: number = 3000;
 
 app.use(async (ctx: any, next: any) => {
   ctx.body = "hello, world.";
   await next();
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server started, listening port:${port}`);
+});
