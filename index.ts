c3e7ee0f98cab73a34f988a1fb1ea187107f669e
Jiawei <liujiaweidreamer@gmail.com>
2020-09-22 18:23:27 +0800

    record response time consuming

--- index.ts
+++ index.ts
@@ -4,7 +4,25 @@ const app = new Koa();
 const port: number = 3000;
 
 app.use(async (ctx: Koa.Context, next: Koa.Next) => {
-  ctx.body = "hello, world.";
+  await next();
+  const consumingTime = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.request.method} - ${ctx.request.url}: ${consumingTime}`);
+});
+
+app.use(async (ctx: Koa.Context, next: Koa.Next) => {
+  const startTime = Date.now();
+  await next();
+  const consumingTime = Date.now() - startTime;
+  ctx.response.set("X-Response-Time", `${consumingTime}ms`);
+});
+
+app.use(async (ctx: Koa.Context, next: Koa.Next) => {
+  let body = "";
+  for (let i = 0; i < 100000; i++) {
+    body += "hello, Koa.\n";
+  }
+  ctx.body = body;
+
   await next();
 });
 
